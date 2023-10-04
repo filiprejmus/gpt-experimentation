5567aabd83d39fbcd4dee51e13770aed65a6506e
filiprejmus <rejmusfilip@gmail.com>
2023-10-04 16:31:15 +0200

    shtuff

--- linkedin-ai/chat.py
+++ linkedin-ai/chat.py
@@ -1,40 +1,39 @@
-from langchain import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.agents import AgentExecutor
-from langchain.callbacks import get_openai_callback
-import os, yaml
-from langchain.agents.agent_toolkits.openapi.spec import reduce_openapi_spec
-import yaml
-from langchain.requests import RequestsWrapper
-from langchain.agents.agent_toolkits.openapi import planner
+import pinecone
+import os
+import openai
+from tqdm.auto import tqdm
+import pandas as pd
+import time
+import json
 
 
-llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
-embeddings = OpenAIEmbeddings()
-user_query = "Use the search endpoint find my meeting with Max and summarize it"
+pc_api_key = os.environ.get("PINECONE_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-with open("notion_api.yaml", "r") as f:
-    notion_yaml = yaml.safe_load(f)
+MODEL = "text-embedding-ada-002"
 
-notion_spec = reduce_openapi_spec(notion_yaml)
+pinecone.init(api_key=pc_api_key, environment="gcp-starter")
+if "openai" not in pinecone.list_indexes():
+    pinecone.create_index("openai", dimension=1536)
 
+index = pinecone.Index("openai")
+# products = pd.read_csv("finger_food.csv")
 
-def construct_notion_auth_headers():
-    key = os.environ.get("NOTION_API_KEY")
-    return {
-        "Authorization": f"Bearer {key}",
-        "accept": "application/json",
-        "Notion-Version": "2022-06-28",
-    }
+# for i in tqdm(range(len(products))):
+#     lines_batch = products.loc[i, "description"]
+#     res = openai.Embedding.create(input=lines_batch, engine=MODEL)
+#     embeds = [record["embedding"] for record in res["data"]]
+#     meta = [{"price": products.loc[i, "price"], "name": products.loc[i, "name"]}]
+#     to_upsert = zip([str(i)], embeds, meta)
+#     index.upsert(vectors=list(to_upsert))
 
+# query = "meats"
+# start_query = time.time()
+# xq = openai.Embedding.create(input=query, engine=MODEL)["data"][0]["embedding"]
+# res = index.query([xq], top_k=5, include_metadata=True)
+# end_query = time.time()
+# for match in res["matches"]:
+#     print(f"{match['score']:.2f}: {match['metadata']['name']}")
 
-headers = construct_notion_auth_headers()
-requests_wrapper = RequestsWrapper(headers=headers)
-
-
-notion_agent = planner.create_openapi_agent(notion_spec, requests_wrapper, llm)
-
-
-with get_openai_callback() as cb:
-    notion_agent.run(user_query)
-    print(cb)
+vector = index.fetch(["0"])["vectors"]["0"]
+print(vector["metadata"])
